9cb9a6ba942bf19fc0e5f4a73f82e0566df5d484
juan.verano <juan.verano@ica.gov.co>
2013-02-13 21:13:10 -0500

    ok

--- views/dowloadFile.php
+++ views/dowloadFile.php
@@ -1,17 +1,17 @@
 <?php
-//include_once '../config/config.php';
-include_once '../components/Components.php';
+include_once '../config/config.php';
+//include_once '../components/Components.php';
 $components = new Components();
 if(isset($_REQUEST["id"])){
     $sql     = "Select * from archivo where idArchivo=".$_REQUEST["id"];
     $rs      = $components->__executeQuery($sql,$components->getConnect());
-    var_dump($rs);
     $row     = mysql_fetch_object($rs);
-    
-    header('Content-type:'. $row->typeFile);
-    header('Content-length:'. $row->size);
-    header('Content-Disposition: attachment; filename='. $row->nameFile);
-    header('Content-Description: PHP Generated Data');
+    @header("Content-type: application/force-download");
+    @header("Content-Transfer-Encoding: Binary");
+    @header('Content-type:'. $row->typeFile);
+    @header('Content-length:'. $row->size);
+    @header('Content-Disposition: attachment; filename='. $row->nameFile);
+    @header('Content-Description: PHP Generated Data');
     echo $row->blobFile;
     
 }
