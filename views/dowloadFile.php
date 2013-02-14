9a99c8a400050645786576163c526a17709e8f51
juan.verano <juan.verano@ica.gov.co>
2013-02-13 21:24:10 -0500

    add changes

--- views/dowloadFile.php
+++ views/dowloadFile.php
@@ -6,13 +6,17 @@ if(isset($_REQUEST["id"])){
     $sql     = "Select * from archivo where idArchivo=".$_REQUEST["id"];
     $rs      = $components->__executeQuery($sql,$components->getConnect());
     $row     = mysql_fetch_object($rs);
-    @header("Content-type: application/force-download");
-    @header("Content-Transfer-Encoding: Binary");
-    @header('Content-type:'. $row->typeFile);
+    //@header("Content-type: application/force-download");
+    //@header("Content-Transfer-Encoding: Binary");
+    header("Content-Type:   application/vnd.ms-excel; charset=utf-8");
+    header("Content-type:   application/x-msexcel; charset=utf-8");
+    
+    //@header('Content-type:'. $row->typeFile);
     @header('Content-length:'. $row->size);
-    @header('Content-Disposition: attachment; filename='. $row->nameFile);
-    @header('Content-Description: PHP Generated Data');
-    echo $row->blobFile;
+    //@header('Content-Disposition: attachment; filename='. $row->nameFile);
+    //@header('Content-Description: PHP Generated Data');
+    //echo $row->blobFile;
+    echo $row->typeFile;
     
 }
 ?>
